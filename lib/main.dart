d468d7fd6a012e753e18105b28e8a41c52444d1a
konark keshaw <konarkkeshaw@gmail.com>
2021-09-19 17:28:52 +0530

    quize app and custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+
     print('Answer chosen!');
+    print(_questionIndex);
   }
 
   @override
@@ -17,15 +33,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text('My first Appp'),
+            title: Text('My App'),
           ),
           body: Column(children: [
-            Text('The questions.'),
-            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            Question(questions[_questionIndex]),
+            RaisedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
             RaisedButton(
                 child: Text('Answer 2'),
                 onPressed: () => {print('Answer 2 is chosen.')}),
-            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 3'), onPressed: _answerQuestion),
           ])),
     );
   }
